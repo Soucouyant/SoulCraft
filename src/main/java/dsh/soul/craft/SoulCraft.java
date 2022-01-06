724938d6ba3f67f5c5e1e3307fd5c3678d83d6cd
ness <duranr@protonmail.com>
2022-01-05 21:03:17 -0500

    Added Divine Fragment, Artifacts (Item Group) and translation file

--- src/main/java/dsh/soul/craft/SoulCraft.java
+++ src/main/java/dsh/soul/craft/SoulCraft.java
@@ -1,21 +1,27 @@
 package dsh.soul.craft;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SoulCraft implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("soulcraft");
+	public static final ItemGroup Artifacts = FabricItemGroupBuilder.build(
+			new Identifier("soulcraft", "artifacts"),
+		() -> new ItemStack(SoulCraft.DIVINE_FRAGMENTS));
+
+	public static final divine_fragments DIVINE_FRAGMENTS  = new divine_fragments(new Item.Settings().group((SoulCraft.Artifacts)));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		Registry.register(Registry.ITEM, new Identifier("soulcraft", "divine_fragments"), DIVINE_FRAGMENTS);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/dsh/soul/craft/divine_fragments.java
@@ -0,0 +1,15 @@
+package dsh.soul.craft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class divine_fragments extends Item {
+    public divine_fragments(Settings settings) {
+        super(settings);
+    }
+
+    // Sounds on equip and usage go here:
+
+}
